ae4c8d82a13081031c3774c91adecaf39b2df06e
AfonsoQueiros13 <afonsoqueiros13@gmail.com>
2019-11-20 09:55:41 +0000

    Changed some files of original repository

--- BitTorrentAPP_Py/BitTorrentApp/lib/bencoding.py
+++ BitTorrentAPP_Py/BitTorrentApp/lib/bencoding.py
@@ -1,22 +1,6 @@
-#
-# pieces - An experimental BitTorrent client
-#
-# Copyright 2016 markus.eliasson@gmail.com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
 from collections import OrderedDict
-
+import time
 
 # Indicates start of integers
 TOKEN_INTEGER = b'i'
@@ -132,6 +116,8 @@ class Decoder:
     def _decode_string(self):
         bytes_to_read = int(self._read_until(TOKEN_STRING_SEPARATOR))
         data = self._read(bytes_to_read)
+        print(data)
+        time.sleep(1)
         return data
 
 
